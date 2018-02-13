44402d496ca06e47eb99b3304032ab2e55fa7e24
usvyaty <yura.usvyat@gmail.com>
2018-02-13 17:41:05 +0000

    cosmetic changes

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,19 +8,17 @@
 #              and compares it with the number of files in teh current directory.  If the input number is different
 #              it displays a message stating if it is bigger or smaller and asks for another number. 
 #              This continues until the numbers match.
-#Run Info      This script runs until the  number match or it is cancelled (Cntr-C)
+#Run Info      This script runs until the  numbers match or it is cancelled (Cntr-C)
 function isvalid_number {
 	if [[ ! $1 || $1 =~ [^0-9] ]]
-        then
-		return 1			
-	else
-		return 0	
+        then return 1			
+	else return 0	
         fi
 }
 function get_number_from_input {
  	local s=${@}
 	read -p "$s" in
-	while ! (isvalid_number $in)
+	while ! (isvalid_number $in) 
 	do
                 echo "Error: '$in' is not a number" > /dev/stderr 
                 read -p "$s" in
@@ -33,12 +31,10 @@ function get_number_files {
 
 cur_dir=$(pwd)
 mess="Guess the number of files in the current directory($cur_dir): "
-
 file_count=$(get_number_files 1)
 let guessed_count=file_count+1
-
-while [ $file_count -ne $guessed_count ]
-do
+while [ $file_count -ne $guessed_count ] 
+	do
 	guessed_count=$(get_number_from_input $mess)
 	[[ $guessed_count -lt file_count ]] && echo "Your guess is too low, please try again"
 	[[ $guessed_count -gt file_count ]] && echo "Your guess is too high, please try again" 
